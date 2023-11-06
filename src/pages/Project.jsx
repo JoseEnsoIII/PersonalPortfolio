b2f12024d8b205edafe1d30d765ec1e086737704
joseenso <joseensothethird@gmail.com>
2023-11-06 13:22:32 +0800

    .

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -331,10 +331,16 @@ export const Projects = () => {
                 Codeigniter3
               </span>
               <span className="shadow-mmd px-5 py-2 rounded-md border-t border-l border-slate-600">
-                PHP SQL
+                PHP
               </span>
               <span className="shadow-mmd px-5 py-2 rounded-md border-t border-l border-slate-600">
-                AJAX JQUERY
+               SQL
+              </span>
+              <span className="shadow-mmd px-5 py-2 rounded-md border-t border-l border-slate-600">
+                AJAX 
+              </span>
+              <span className="shadow-mmd px-5 py-2 rounded-md border-t border-l border-slate-600">
+              JQUERY
               </span>
             </div>
             <div className="flex gap-5 items-center justify-center"></div>
