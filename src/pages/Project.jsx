100ae71a4e88a240cc0db37b1c3170b604f59e13
josh-delfin <123255126+josh-delfin@users.noreply.github.com>
2024-01-31 19:34:55 +0800

    Change Img for Figma

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -449,14 +449,15 @@ export const Projects = () => {
               >
                 <img
                   className="w-full h-full object-cover rounded-xl shadow-mmd border-t border-l border-slate-600"
-                  src="/images/z.PNG"
+                  src="/images/111.PNG"
                   alt=""
                 />
               </motion.div>
             </a>
           </div>
           <div className="w-full xl:mt-0 sm:mt-5 flex flex-col gap-5   ">
-            <h1 className="text-center font-bold text-[20px] ">Web Design</h1>
+            <h1 className="text-center font-bold text-[20px] ">UI/UX Web Design</h1>
+           
             <motion.p
               initial={{ opacity: 0, x: 50 }}
               whileInView={{ opacity: 1, x: 0 }}
@@ -464,22 +465,22 @@ export const Projects = () => {
               viewport={{ once: true }}
               className="text-center text-gray-500"
             >
-              Using Figma
-            </motion.p>
-            <motion.p
-              initial={{ opacity: 0, x: 50 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5, type: "tween", delay: 0.2 }}
-              viewport={{ once: true }}
-              className="text-center text-gray-500"
-            >
-              I created my own version of an SMS eLearning website's UI/UX
-              design using Figma.
+              Web Design of my Work Project In Wordpress
             </motion.p>
             <div className="flex gap-5 justify-center">
               <span className="shadow-mmd px-5 py-2 rounded-md border-t border-l border-slate-600 ">
                 FIGMA
               </span>
+            </div>
+            <div className="flex gap-5 items-center justify-center">
+              <a
+                className="flex font-semibold items-center justify-between gap-2 "
+                href="https://www.figma.com/file/UcHOWo37hRVMW7zjsXvtGj/Untitled?type=design&mode=design&t=NeWG2puztmo7jwqG-0"
+                target="_blank"
+              >
+                Live Demo{" "}
+                <i className="fa-solid fa-share-from-square text-2xl"></i>
+              </a>
             </div>
             <div className="flex gap-5 items-center justify-center"></div>
           </div>
