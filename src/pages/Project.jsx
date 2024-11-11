73b65d1db6ad79cfce86ee65aab3a8367c474aa7
josh-delfin <123255126+josh-delfin@users.noreply.github.com>
2024-11-11 22:11:35 +0800

    .

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -144,22 +144,18 @@ export const Projects = () => {
               <span className="shadow-mmd px-5 py-2 rounded-md border-t border-l border-slate-600">
                 CMS
               </span>
-
-
             </div>
             <div className="flex gap-5 justify-center">
               <span className="shadow-mmd px-5 py-2 rounded-md border-t border-l border-slate-600">
                 Elementor/Divi
               </span>
-              <span className="shadow-mmd px-5 py-2 rounded-md border-t border-l border-slate-600">
-                GoHighLevel (GHL)
-              </span>
-
             </div>
             <div className="flex gap-5 justify-center">
-
+ <span className="shadow-mmd px-5 py-2 rounded-md border-t border-l border-slate-600">
+          GHL
+              </span>
               <span className="shadow-mmd px-5 py-2 rounded-md border-t border-l border-slate-600">
-                Funnel Building
+                Funnel
               </span>
             </div>
             <div className="flex gap-5 items-center justify-center">
